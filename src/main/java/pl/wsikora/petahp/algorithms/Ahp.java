758284f2bd520de5c699e2543fa6ef63fc820e88
W-Sikora <242612@student.pwr.edu.pl>
2020-05-25 22:21:31 +0200

    added Analytic Hierarchy Process algorithm

--- src/main/java/pl/wsikora/petahp/algorithms/Ahp.java
+++ src/main/java/pl/wsikora/petahp/algorithms/Ahp.java
@@ -1,123 +1,173 @@
 package pl.wsikora.petahp.algorithms;
 
-import java.lang.reflect.Array;
 import java.util.*;
 
 public class Ahp {
-    private int noOfCriteria;
-    private double[][] matrix;
+    private List<Double> list;
+    private int dimension;
+    private double[][] array;
 
-    public Ahp() {
+    public Ahp(List<Double> list) {
+        this.list = list;
     }
 
-    public Ahp(int noOfCriteria, double[][] matrix) {
-        this.noOfCriteria = noOfCriteria;
-        this.matrix = matrix;
+    public void initialize() {
+        dimension = calcDimension();
+        array = new double[dimension][dimension];
+        complete();
     }
 
-    public int getNoOfCriteria() {
-        return noOfCriteria;
+    private int calcDimension() {
+        int a = 1, b = -1, c = -2 * list.size();
+        return (int) ((-b + Math.sqrt(b * b - (4 * a * c))) / 2 * a);
     }
 
-    public double[][] getMatrix() {
-        return matrix;
+    public StringBuilder show1() {
+        StringBuilder result = new StringBuilder("[ ");
+        for (int i = 0; i < dimension ; i++) {
+            if (i == dimension - 1) {
+                result.append(Arrays.toString(array[i]));
+            } else {
+                result.append(Arrays.toString(array[i]) + ", ");
+            }
+        }
+        result.append(" ]");
+        return result;
     }
 
-    public void setNoOfCriteria(int noOfCriteria) {
-        this.noOfCriteria = noOfCriteria;
-        this.matrix = createMatrix();
-    }
-
-    public void showMatrix() {
-        StringBuilder result = new StringBuilder("matrix values in this view have been approximated\n[");
-        for (int i = 0; i < noOfCriteria; i++) {
+    public StringBuilder show2() {
+        StringBuilder result = new StringBuilder("[");
+        for (int i = 0; i < dimension; i++) {
             result.append("\t[");
-            for (int j = 0; j < noOfCriteria; j++) {
-                String strDouble = String.format("%.3f", matrix[i][j]);
-                result.append(strDouble);
-                if (j < noOfCriteria - 1) {
-                    result.append("\t");
+            for (int j = 0; j < dimension; j++) {
+                result.append(array[i][j]);
+                if (j < dimension - 1) {
+                    result.append(",\t");
                 }
             }
-            if (i < noOfCriteria - 1) {
-                result.append("]\n");
+            if (i < dimension - 1) {
+                result.append("],\n");
             } else {
                 result.append("]");
             }
         }
         result.append("\t]\n");
-        System.out.println(result);
+        return result;
     }
 
-    public double[][] createMatrix() {
-        double[][] array = new double[noOfCriteria][noOfCriteria];
-        for (int i = 0; i < noOfCriteria; i++) {
-            for (int j = 0; j < noOfCriteria; j++) {
+    private void complete() {
+        List<Double> copy = new ArrayList<>(list);
+        for (int i = 0; i < dimension; i++) {
+            for (int j = 0; j < dimension; j++) {
                 if (i == j) {
                     array[i][j] = 1;
                 }
-            }
-        }
-        return array;
-    }
-
-    public void completeMatrix(List<Double> preferences) {
-        for (int i = 0; i < noOfCriteria; i++) {
-            for (int j = 0; j < noOfCriteria; j++) {
                 if (i < j) {
-                    matrix[i][j] = preferences.get(0);
-                    preferences.remove(0);
+                    array[i][j] = copy.get(0);
+                    copy.remove(0);
                 }
                 if (i > j) {
-                    matrix[i][j] = 1 / matrix[j][i];
+                    array[i][j] = 1 / array[j][i];
                 }
             }
         }
     }
 
-    public void matrixCorrection(List<Double> sums) {
-        for (int i = 0; i < noOfCriteria; i++) {
-            for (int j = 0; j < noOfCriteria; j++) {
-                matrix[i][j] /= sums.get(i);
-            }
+    private double[] rowsColumnsSums(double[][] arr, boolean rowsOrColumns) {
+        double[][] doubles = new double[dimension][dimension];
+        for (int i = 0; i < dimension; i++) {
+            doubles[i] = arr[i].clone();
         }
-    }
 
-    public List<Double> getSumOfEach(boolean columnOrRow) {
-        List<Double> sums = new ArrayList<>();
-        double sum = 0;
-        for (int i = 0; i < noOfCriteria; i++) {
-            for (int j = 0; j < noOfCriteria; j++) {
-                if (columnOrRow) {
-                    sum = sum + matrix[j][i];
+        double[] totalSums = new double[doubles.length];
+        for (int i = 0; i < doubles.length; ++i) {
+            double sum = 0;
+            for (int j = 0; j < doubles[0].length; ++j) {
+                if (rowsOrColumns) {
+                    sum += doubles[i][j];
                 } else {
-                    sum = sum + matrix[i][j];
+                    sum += doubles[j][i];
                 }
             }
-            sums.add(sum);
-            sum = 0;
+            totalSums[i] = sum;
         }
-        return sums;
+        return totalSums;
     }
 
-    public List<Double> getMeanOfEachRow() {
-        List<Double> means = new ArrayList<>();
-        for (Double sum : getSumOfEach(false)) {
-            means.add(sum / noOfCriteria);
+    public double[] calcWeights() {
+        double[][] doubles = new double[dimension][dimension];
+        for (int i = 0; i < dimension; i++) {
+            doubles[i] = array[i].clone();
         }
-        return means;
+
+        double[] weights = new double[dimension];
+        double[] columnsSums = rowsColumnsSums(array, false);
+
+        for (int i = 0; i < dimension; ++i) {
+            for (int j = 0; j < dimension; ++j) {
+                doubles[i][j] /= columnsSums[j];
+            }
+        }
+
+        double[] rowsSums = rowsColumnsSums(doubles, true);
+
+        for (int i = 0; i < dimension; i++) {
+            weights[i] = rowsSums[i] / dimension;
+        }
+        return weights;
     }
 
-    public List<Double> getWeights(List<Double> preferences) {
-        completeMatrix(preferences);
-        matrixCorrection(getSumOfEach(true));
-        return getMeanOfEachRow();
+    public double calcLambdaMax() {
+        double[] weights = calcWeights();
+        double[][] doubles = new double[dimension][dimension];
+        for (int i = 0; i < dimension; i++) {
+            doubles[i] = array[i].clone();
+        }
+
+        for (int i = 0; i < dimension; i++) {
+            for (int j = 0; j < dimension ; j++) {
+                doubles[i][j] *= weights[j];
+            }
+        }
+        double[] lambdas = rowsColumnsSums(doubles, true);
+        double sum = 0;
+        for (int i = 0; i < dimension; i++) {
+            sum += lambdas[i] / weights[i];
+        }
+        return sum / dimension;
     }
 
+    public double calcConsistencyIndex() {
+        return (calcLambdaMax() - dimension) / (dimension - 1);
+    }
 
+    public double calcConsistencyRatio() {
+        double[] randomIndex = {0.,0.,0.58,0.90,1.12,1.24,1.32,1.41,1.45,1.49,1.51,1.54,1.56,1.57,1.58};
+        return calcConsistencyIndex() / randomIndex[dimension - 1];
+    }
 
-    public boolean checkJudgments(double consistencyRatio) {
-        return consistencyRatio < 0.1;
+    public boolean examineConsistency() {
+        return calcConsistencyRatio() < 0.1;
+    }
+
+    public StringBuilder summarizeResults() {
+        StringBuilder result = new StringBuilder("Ahp summary {");
+        result.append("\n\tweights = " + Arrays.toString(calcWeights()));
+        result.append(",\n\tlambda max = " + calcLambdaMax());
+        result.append(",\n\tconsistency index = " + calcConsistencyIndex());
+        result.append(",\n\tconsistency ratio = " + calcConsistencyRatio());
+        result.append(",\n\tis consistent = " + examineConsistency());
+        result.append("\n}");
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Ahp {" +
+                "\n\tlist of preferences = " + list.toString() +
+                ",\n\tdimension = " + dimension + " x " + dimension +
+                ",\n\tarray = " + show1() +
+                "\n}";
     }
 
 }
