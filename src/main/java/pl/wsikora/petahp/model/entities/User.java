0e8ff9e025d8925e2f5e9d502f37cff4cf201630
W-Sikora <242612@student.pwr.edu.pl>
2021-08-25 18:58:12 +0200

    updated user entity

--- src/main/java/pl/wsikora/petahp/model/entities/User.java
+++ src/main/java/pl/wsikora/petahp/model/entities/User.java
@@ -1,10 +1,13 @@
 package pl.wsikora.petahp.model.entities;
 
 import org.springframework.format.annotation.DateTimeFormat;
-import pl.wsikora.petahp.utils.Builder;
+import pl.wsikora.petahp.utils.BuilderTemplate;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
 
 @Entity
 @Table(name = "users")
@@ -25,46 +28,20 @@ public class User {
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime creationDate;
 
+    private boolean enabled;
+
     @Deprecated
     public User() {
     }
 
-    public static class UserBuilder extends Builder<User> {
-
-        protected UserBuilder() {
-            super(new User());
-        }
-
-        public UserBuilder withId(long id) {
-            add(e -> e.id = id);
-            return this;
-        }
-
-        public UserBuilder withName(String name) {
-            add(e -> e.name = name);
-            return this;
-        }
-
-        public UserBuilder withEmail(String email) {
-            add(e -> e.email = email);
-            return this;
-        }
-
-        public UserBuilder withPassword(String password) {
-            add(e -> e.password = password);
-            return this;
-        }
-
-        public UserBuilder withCreationDate(LocalDateTime creationDate) {
-            add(e -> e.creationDate = creationDate);
-            return this;
-        }
-
+    public static Builder builder() {
+        return new Builder(new User());
     }
 
     @PrePersist
     public void prePersist() {
         creationDate = LocalDateTime.now();
+        enabled = true;
     }
 
     public long getId() {
@@ -87,14 +64,27 @@ public class User {
         return creationDate;
     }
 
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", email='" + email + '\'' +
-                ", creationDate=" + creationDate +
-                '}';
+    public static class Builder extends BuilderTemplate<User> {
+
+        private Builder(User user) {
+            super(user);
+        }
+
+        public Builder withName(String name) {
+            add(e -> e.name = name);
+            return this;
+        }
+
+        public Builder withEmail(String email) {
+            add(e -> e.email = email);
+            return this;
+        }
+
+        public Builder withPassword(String password) {
+            add(e -> e.password = password);
+            return this;
+        }
+
     }
 
 }
--- src/main/java/pl/wsikora/petahp/utils/BuilderTemplate.java
+++ src/main/java/pl/wsikora/petahp/utils/BuilderTemplate.java
@@ -4,11 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
 
-public abstract class Builder<T> {
+public abstract class BuilderTemplate<T> {
     private final List<Consumer<T>> operations;
     private final T object;
 
-    protected Builder(T object) {
+    protected BuilderTemplate(T object) {
         this.object = object;
         this.operations = new ArrayList<>();
     }
